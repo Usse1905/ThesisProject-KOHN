cc45fdfc64dd557ea7a8846db068a323e591346e
Ussema1905 <ousema.1905@gmail.com>
2024-10-10 17:41:33 +0100

    Component structure, server, DB connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,13 @@
 import React,{useState} from "react";
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import "./App.css";
+import LogIn from "./Components/LogIn";
+import SignUp from "./Components/SignUp";
+import MainPageC from "./Components/Company/MainPageC";
+import MainPageUser from "./Components/User/MainPageUser";
+import CarDetails from "./Components/User/CarDetails";
+import AddCar from "./Components/Company/AddCar";
+import ProfilePage from "./Components/Company/ProfilePage";
 
 const App = () => {
   return (
@@ -8,8 +15,13 @@ const App = () => {
   <Navbar/>
   <div >
   <Routes>
-    <Route />
-    
+    <Route path="/login" element={<LogIn/>}/>
+    <Route path="/signup" element={<SignUp/>}/>
+    <Route path="/User/Mainpage" element={<MainPageUser/>}/>
+    <Route path="/User/CarDetails" element={<CarDetails/>}/>
+    <Route path="/Company/AddCar" element={<AddCar/>}/>
+    <Route path="/Company/Mainpage" element={<MainPageC/>}/>
+    <Route path="/Company/Profile" element={<ProfilePage/>}/>
   </Routes>
   </div>
   </BrowserRouter>
---
+++ client/src/Components/Company/MainPageC.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainPageC = () => {
+  return (
+    <div>MainPageC</div>
+  )
+}
+
+export default MainPageC
